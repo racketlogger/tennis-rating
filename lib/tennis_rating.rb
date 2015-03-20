674826b49154810ddcd465c9db9c5a8c42fbfc04
Sourav Moitra <sourav.moitr@gmail.com>
2015-03-20 10:01:34 +0530

    rewrite of points with help of tennis gem

--- lib/tennis_rating.rb
+++ lib/tennis_rating.rb
@@ -1,19 +1,58 @@
 require 'yaml'
-results = YAML.load_file("sample-matches.yaml") # getting all the results in a array
-results.each do |result| # iterating through each item which is a string
-  array = result.gsub(/[^0-9\s]/, '').split(" ") # removing every character except numbers and space and then splitting with space
-  twoDArray = array.map do |s| #making a 2D array of each set results
-     s.split("").map{ |d| d.to_i }
-   end
-  if twoDArray.length # if array is not empty
-    points = 0
-    twoDArray.each do |sub| # iterating through each game set
-      p sub #printing of result set
-      if sub[0] > sub[1] # if the set is won by the player that is 1st no is greater than 2nd
-        points += 2 - (sub[1].to_f / sub[0].to_f) # give players 2 point minus ratio of lost
+require "tennis"
+##
+# This class inherits from tennis class and adds points
+#
+class TennisRatings < Tennis
+
+  attr_reader :points
+
+  def initialize(score)
+    super
+    give_points
+  end
+
+ private
+
+  def give_points
+    point_0 = 0
+    point_1 = 0
+    @set_winners.each_with_index do |winner, set|
+      if winner == 1
+        point_1 = calculate_points(set, 1, 0)
+      else
+        point_0 = calculate_points(set, 0, 1)
       end
+      @points = [point_0, point_1]
     end
   end
-  puts "points: #{points.round(2)}" # printing of points
-  puts "* * * *"
+
+  def calculate_points(set, winner, loser)
+    ((2 - @score[set][loser].to_f / @score[set][winner].to_f) * 100).round
+  end
+
 end
+
+##
+# Method to read data from yaml file and return an array
+#
+def read_yaml(file_name = "sample-matches.yaml")
+  results = YAML.load_file(file_name) # getting all the results in a array
+end
+
+##
+# Method for checking
+#
+
+def give_ratings(result_set)
+  read_yaml.each do |score|
+    begin
+      p TennisRatings.new(score)
+    rescue ArgumentError
+      puts "#{score} : Not a match result"
+    end
+  end
+end
+
+
+give_ratings(read_yaml)
