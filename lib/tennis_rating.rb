8e3676965b951c5bff71c9a213ed399828ac74e0
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-05 11:57:54 +0530

    intial iteration

--- lib/tennis_rating.rb
+++ lib/tennis_rating.rb
@@ -1,93 +1,30 @@
-require 'yaml'
 require "tennis"
-##
-# This class inherits from tennis class and adds points
-#
-class TennisRatings < Tennis
 
-  attr_reader :points
-
-  def initialize(score)
-    super(score)
-    give_points
-  end
-
-  def give_points
-    calculate_all
-  end
-
-  private
-
-
-  def calculate_all
-    @points = []
-    calculate_points_zero
-    calculate_points_one
-    calculate_points_two
-    calculate_points_three
-  end
-
-  def calculate_points_zero
-    point_0 = 0
-    point_1 = 0
-    @set_winners.each_with_index do |winner, set|
-      if winner == 1
-        point_1 = algorithm0(set, 1, 0)
+  def points_for(scores)
+    raise "Scores is not an array" unless scores.instance_of? Array
+    tennis = Array.new
+    scores.each do |score|
+      tennis << Tennis.new(score)
+    end
+    final_score = Array.new
+    tennis.each do |match|
+      winner = match.winner
+      if winner === 0
+        if match.sets_lost.first === 0
+          final_score << [14, 6]
+        else
+          final_score << [12, 8]
+        end
       else
-        point_0 = algorithm0(set, 0, 1)
+        if match.sets_lost.first === 0
+          final_score << [6, 14]
+        else
+          final_score << [8, 12]
+        end
       end
     end
-    @points << [point_0, point_1]
+    final_score
   end
 
-  def calculate_points_one
-    point_0 = 0
-    point_1 = 0
-    @set_winners.each_with_index do |winner, set|
-      if winner == 1
-        point_1 = algorithm1(set, 1, 0)
-      else
-        point_0 = algorithm1(set, 0, 1)
-      end
-    end
-    @points << [point_0, point_1]
-  end
-
-  def calculate_points_two
-    point_0 = 0
-    point_1 = 0
-    if winner == 1
-      point_1 = algorithm2(1, 0)
-    else
-      point_0 = algorithm2(0, 1)
-    end
-    @points << [point_0, point_1]
-  end
-
-  def calculate_points_three
-    point_0 = 0
-    point_1 = 0
-    if winner == 1
-      point_1 = algorithm3(1)
-    else
-      point_0 = algorithm3(0)
-    end
-    @points << [point_0, point_1]
-  end
-
-  def algorithm0(set, winner, loser)
-    ((2 - @score[set][loser].to_f / @score[set][winner].to_f) * 100).round
-  end
-
-  def algorithm1(set, winner, loser)
-    ((1 - @score[set][loser].to_f / @score[set][winner].to_f) * 100).round
-  end
-
-  def algorithm2(winner, loser)
-    ((@score.length  - @games_won[loser].to_f / @games_won[winner].to_f) * 100).round
-  end
-
-  def algorithm3(winner)
-    2 * @sets_won.first
-  end
-end
+scores = ["6-4, 6-2", "6-2, 4-6, 5-7", "3-6, 6-2, 6-2",  "6-3, 6-3"]
+p points_for("")
