234f85ec521cbd86c068af64a9e6728ce8f5f83b
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-05 16:20:30 +0530

    spelling change of loser

--- lib/tennis_rating.rb
+++ lib/tennis_rating.rb
@@ -8,31 +8,31 @@ require "tennis"
     end
     final_score = Array.new
     tennis.each do |match|
-      looser = winner = 0
+      loser = winner = 0
       total_sets =  match.sets_won.inject(:+)
       if total_sets === 3
         winner = 12
       else
         winner = 14
       end
-      looser = looser_max(match)
+      loser = loser_max(match)
       if match.winner === 0
-        final_score << [winner, looser]
+        final_score << [winner, loser]
       else
-        final_score << [looser, winner]
+        final_score << [loser, winner]
       end
     end
     final_score
   end
 
 
-  def looser_max(match)
-    looser = 0
-    looser = 1 if match.winner === 0
-    looser_game = match.score.sort_by { |score| score[looser] }
-    total_looser_score = looser_game[0][looser] + looser_game[1][looser]
-    total_looser_score = 8 if total_looser_score > 8
-    total_looser_score
+  def loser_max(match)
+    loser = 0
+    loser = 1 if match.winner === 0
+    loser_game = match.score.sort_by { |score| score[loser] }
+    total_loser_score = loser_game[0][loser] + loser_game[1][loser]
+    total_loser_score = 8 if total_loser_score > 8
+    total_loser_score
   end
 
 scores = ["6-4, 6-2", "6-2, 4-6, 5-7", "3-6, 6-2, 6-2",  "6-3, 6-3", "6-1, 6-1", "1-6, 1-6"]
