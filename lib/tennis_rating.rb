caaa68e1c55695fc76e45b9b6d8a96002c820a35
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-06 10:32:44 +0530

    Rating system

--- lib/tennis_rating.rb
+++ lib/tennis_rating.rb
@@ -3,7 +3,7 @@ require "tennis"
 class RacketLoggerUT
 
   attr_reader :points
-
+  attr_reader :rating
 
   def initialize(scores)
     raise "Scores is not an array" unless scores.instance_of? Array
@@ -24,6 +24,7 @@ class RacketLoggerUT
       final_score  << score
     end
     @points = final_score
+    calculate_rating
   end
 
 private
@@ -37,4 +38,15 @@ private
     total_loser_score
   end
 
+  def calculate_rating
+    summation = 0
+    @points.each do |point|
+      summation += point[0] - point[1]
+    end
+    @rating = (summation.to_f / @points.length.to_f).round(2)
+  end
 end
+
+# a = RacketLoggerUT.new(["7-5, 5-7, 6-2", "4-6, 1-6", "7-5, 6-2", "3-6, 6-4, 6-2", "6-4, 6-2", "6-1, 6-3", "6-2, 6-4"])
+# a.rating #=> 4.57
+#a.points #=> [[12, 8], [5, 14], [14, 7], [12, 8], [14, 6], [14, 4], [14, 6]]
