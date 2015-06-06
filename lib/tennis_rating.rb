0a69538ca2cc940111ba7fa1be88a156d383ed10
Sourav Moitra <sourav.moitr@gmail.com>
2015-06-06 09:49:04 +0530

    Created racketlogger class

--- lib/tennis_rating.rb
+++ lib/tennis_rating.rb
@@ -1,6 +1,11 @@
 require "tennis"
 
-  def points_for(scores)
+class RacketLoggerUT
+
+  attr_reader :points
+
+
+  def initialize(scores)
     raise "Scores is not an array" unless scores.instance_of? Array
     tennis = Array.new
     scores.each do |score|
@@ -18,10 +23,10 @@ require "tennis"
       score = [winner, loser] if match.winner === 0
       final_score  << score
     end
-    final_score
+    @points = final_score
   end
 
-
+private
   def loser_max(match)
     loser = 0
     return 0 if match.default?
@@ -32,10 +37,4 @@ require "tennis"
     total_loser_score
   end
 
-scores = ["6-4, 6-2", "6-2, 4-6, 5-7", "3-6, 6-2, 6-2",  "6-3, 6-3", "6-1, 6-1", "1-6, 1-6"]
-p points_for(["7-5, 5-7, 6-2", "4-6, 1-6", "7-5, 6-2", "3-6, 6-4, 6-2", "6-4, 6-2",  "6-1, 6-3", "6-2, 6-4",
-              "7-6, 5-7, 6-2", "6-4, 6-1", "6-2, 6-4", "4-6, 6-0, 6-3", "4-6, 6-3, 7-6", "6-1, 6-3", "2-6, 6-4, 0-6",
-              "6-4, 6-2", "5-7, 2-6", "6-4, 7-5", "6-2, 6-2", "7-5, 1-6, 3-6", "1-6, 0-6"])
-
-
-#[[12, 8], [5, 14], [14, 7], [12, 8], [14, 6], [14, 4], [14, 6], [12, 8], [14, 5], [14, 6], [12, 8], [12, 8], [14, 4], [8, 12], [14, 6], [7, 14], [14, 8], [14, 4], [8, 12], [1, 14]]
+end
